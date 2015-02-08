b28873ce4f4b6dc3011a8511be4d880481c00832
shilpa <shilparoys@yahoo.com>
2015-02-08 15:31:21 -0800

    output

--- CS172/WebCrawler.java
+++ CS172/WebCrawler.java
@@ -120,32 +120,20 @@ public class WebCrawler{
     }
   }
 
-	int j = 0;
 	public void outputToFile(String fileName2){
 		try{
-			j++;
 			File theDir = new File(fileName2);
 			if(!theDir.exists()){
 				theDir.mkdirs();
 			}
-			Iterator it = url.iterator();
-			while(it.hasNext()){
-				String temp = it.next().toString();
-				URL urlObj = new URL(temp);
-				BufferedReader x = new BufferedReader(new InputStreamReader(urlObj.openConnection().getInputStream()));
-				String fileName = "file" + i + ".html";
-				BufferedWriter fos = new BufferedWriter(new FileWriter(new File(theDir, fileName)));
-				while(x.ready() && keepgoing){
-						String line = x.readLine();
-						fos.write(line);
-						fos.write("\n");
-					}
-				x.close();
-				fos.close();
-
-			}
-		}
-		catch(MalformedURLException ex){
+            String fileName = "answers.txt";
+            BufferedWriter writer = new BufferedWriter(new FileWriter("answers.txt"));
+            Iterator<String> iterator = url.iterator(); 
+			while(iterator.hasNext()){
+                String temp = iterator.next().toString();
+                writer.write(temp + "\n");
+            }
+            writer.close();
 		}
 		catch(IOException e){
 				System.err.format("IO exception at downloadfile");
